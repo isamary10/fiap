67e12a2fcd8d69514de476926324ea56ef1227a1
Isa Mary <isa.kusuki@gmail.com>
2022-03-21 11:54:30 -0300

    Declara variáveis e faz interpolação com printf

--- domain_driven_design/eclipse-fiap-ddd/PrimeiraAula/src/aula_um/HelloWorld.java
+++ domain_driven_design/eclipse-fiap-ddd/PrimeiraAula/src/aula_um/HelloWorld.java
@@ -3,9 +3,15 @@ package aula_um;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// declaração de variável
+		String nome = "Isa Mary";
+		int idade = 35;
+		
 		System.out.println("Hello World!!!");
-		System.out.println("Meu nome é Isa Mary");
+		// concatenação com printf
+		System.out.printf("Meu nome é %s e eu tenho %d anos", nome, idade);
+		// concatenação com +
+		//System.out.println("Meu nome é " + nome + " e eu tenho " + idade + " anos");
 	}
 
 }
