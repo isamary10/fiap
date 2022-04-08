894749d1c94e3eac587745875fd0beb6382ffad1
Isa Mary <isa.kusuki@gmail.com>
2022-04-08 13:42:57 -0300

    Feito exercícios de Estrutura Sequencial e Estrutura Condicional

---
+++ computational_thinking_using_python/exercicios_extras/estrutura_sequencial/01_idade.py
@@ -0,0 +1,7 @@
+idade = int(input('Digite sua idade: '))
+idade_meses = idade * 12
+idade_dias = idade_meses * 30
+
+print('Idade em anos: ', idade)
+print('Idade em meses: ', idade_meses)
+print('Idade em dias: ', idade_dias)
