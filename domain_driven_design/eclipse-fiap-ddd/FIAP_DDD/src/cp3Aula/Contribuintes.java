7ebaa9c9d7dc1be3719df32b642596e44b02aac7
Isa Mary <isa.kusuki@gmail.com>
2022-06-06 23:08:11 -0300

    Resolvido CP3 em aula

--- domain_driven_design/eclipse-fiap-ddd/FIAP_DDD/src/cp3Aula/Contribuintes.java
+++ domain_driven_design/eclipse-fiap-ddd/FIAP_DDD/src/cp3Aula/Contribuintes.java
@@ -1,11 +1,21 @@
 package cp3Aula;
 
+import java.util.ArrayList;
+
 public class Contribuintes {
-	public void adicionarContribuintes(Contribuinte c) {
-		//Adicionar um Contribuinte no vetor
-		
-		for(int i=0; i<contribuintes.length; i++) {
-			System.out.println(contribuintes[i].toString());
-		}
-	}
+    ArrayList<Contribuinte> listaContrib = new ArrayList<Contribuinte>();
+
+    public Contribuintes() {
+
+    }
+
+    public void adicionarContribuinte(Contribuinte c) {
+        listaContrib.add(c);
+    }
+
+    public void apresentarContribuinte() {
+        for (int i = 0; i < listaContrib.size(); i++) {
+            System.out.println(listaContrib.get(i).toString() + "\n");
+        }
+    }
 }
--- domain_driven_design/eclipse-fiap-ddd/FIAP_DDD/src/cp3Aula/TesteContribuinte.java
+++ domain_driven_design/eclipse-fiap-ddd/FIAP_DDD/src/cp3Aula/TesteContribuinte.java
@@ -3,24 +3,24 @@ package cp3Aula;
 public class TesteContribuinte {
 
 	public static void main(String[] args) {
+		Contribuintes contribuinte = new Contribuintes();
+		
 		PessoaFisica pf1 = new PessoaFisica("PF 1", 1000, "123.456.789-10");
 		PessoaFisica pf2 = new PessoaFisica("PF 2", 2000, "124.457.710-11");
-		PessoaFisica pf3 = new PessoaFisica("PF 3", 3000, "125.458.711-12");
+		PessoaFisica pf3 = new PessoaFisica("PF 3", 4000, "125.458.711-12");
 		
 		
 		PessoaJuridica pj1 = new PessoaJuridica("PJ1", 8000, "12.345.678/0001-50");
 		PessoaJuridica pj2 = new PessoaJuridica("PJ2", 9000, "13.346.679/0001-60");
 		PessoaJuridica pj3 = new PessoaJuridica("PJ3", 10000, "14.347.610/0001-70");
 		
-		Contribuinte[] contribuintes = new Contribuinte[6];
-		contribuintes[0] = pf1;
-		contribuintes[1] = pf2;
-		contribuintes[2] = pf3;
-		contribuintes[3] = pj1;
-		contribuintes[4] = pj2;
-		contribuintes[5] = pj3;
-		
-		
+		contribuinte.adicionarContribuinte(pf1);
+		contribuinte.adicionarContribuinte(pf2);
+		contribuinte.adicionarContribuinte(pf3);
+		contribuinte.adicionarContribuinte(pj1);
+		contribuinte.adicionarContribuinte(pj2);
+		contribuinte.adicionarContribuinte(pj3);
+		contribuinte.apresentarContribuinte();
 	}
 
 }
